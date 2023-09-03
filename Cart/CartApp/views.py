89adb21d2e1b2c3ce85c4540af0f2e2e89c7394b
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-09-02 23:23:47 -0500

    post de carta

--- Cart/CartApp/views.py
+++ Cart/CartApp/views.py
@@ -10,3 +10,12 @@ class CartItemAPIView(APIView):
         items = CartItem.objects.filter(user=user_id)
         serializer = CartItemSerializer(items, many=True)
         return Response(serializer.data)
+    
+    def post(self,request):
+        user_id = request.data.get('user')  #Obtener el ID de usuario enviado desde Node.js
+        cart_id = request.data.get('cart_id') #Obtener el ID de la carta enviado desde Node.js
+        data = {
+            'user': user_id,
+            'title': request.data.get('title', ''),
+            'description': request.data.get('description', '')
+        }
